1093f3b8501aceca300602b00e6e7f98a84824fd
Marek Kijo <marekkijo@gmail.com>
2023-05-16 04:18:26 +0200

    Wolf: Refactoring - MapRenderer

--- projects/wolf/map_renderer.hpp
+++ projects/wolf/map_renderer.hpp
@@ -7,9 +7,9 @@
 #include "tools/sdl/sdl_system.hpp"
 
 #include <glm/mat4x4.hpp>
+#include <glm/vec3.hpp>
 
 #include <memory>
-#include <tuple>
 
 namespace wolf {
 class MapRenderer final : public Renderer {
@@ -33,21 +33,13 @@ private:
   std::shared_ptr<const VectorMap>   vector_map_{};
   std::shared_ptr<const PlayerState> player_state_{};
 
-  bool  player_oriented_{false};
-  float translate_x_{};
-  float translate_y_{};
-  float scale_{};
+  bool      player_oriented_{false};
+  float     scale_{};
+  glm::vec3 screen_center_translation_{};
 
-  [[nodiscard]] const VectorMap         &vector_map() const;
-  [[nodiscard]] const PlayerState       &player_state() const;
-  [[nodiscard]] bool                     player_oriented() const;
-  [[nodiscard]] float                    translate_x() const;
-  [[nodiscard]] float                    translate_y() const;
-  [[nodiscard]] std::tuple<float, float> translate() const;
-  [[nodiscard]] float                    scale() const;
-  void                                   redraw_player_oriented() const;
-  void                                   redraw_map_oriented() const;
-  void                                   draw_map(const glm::mat4 &mat) const;
-  void                                   draw_player(const glm::mat4 &mat) const;
+  void redraw_player_oriented() const;
+  void redraw_map_oriented() const;
+  void draw_map(const glm::mat4 &mat) const;
+  void draw_player(const glm::mat4 &mat) const;
 };
 } // namespace wolf
