75efc5506c15f1efcd4f9a24aec4d65026e82fbf
Marek Kijo <marekkijo@gmail.com>
2024-02-14 03:14:43 +0100

    OpenGLStreaming: Basic streaming

--- projects/streaming/opengl_streamer/codec.cpp
+++ projects/streaming/opengl_streamer/codec.cpp
@@ -1,13 +1,15 @@
 #include "codec.hpp"
 
 namespace streaming {
-Codec::Codec() : output_packet_{av_packet_alloc()} {
+Codec::Codec(int width, int height) : output_packet_{av_packet_alloc()} {
   context_.filename = "/home/marek/works/output.mp4";
   avformat_alloc_output_context2(&context_.avfc, nullptr, nullptr, context_.filename.c_str());
   context_.video_avs  = avformat_new_stream(context_.avfc, nullptr);
   context_.video_avc  = avcodec_find_encoder_by_name("libx264");
   context_.video_avcc = avcodec_alloc_context3(context_.video_avc);
   av_opt_set(context_.video_avcc->priv_data, "preset", "fast", 0);
+  context_.video_avcc->width  = width;
+  context_.video_avcc->height = height;
 }
 
 Codec::~Codec() {
